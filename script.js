069c2cc06a48b6ed35bd83756e741a4996fe230b
Dikesh Kumar <117905520+Dikesh01@users.noreply.github.com>
2023-06-18 19:34:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	width;
+	height;
+	constructor(width, height){
+		this.width = width;
+		this.height= height;
+	}
+	get width(){
+		return this.width;
+	}
+	get height(){
+		return this.height;
+	}
+	getArea(){
+		return (this.width*this.height);
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	getPerimeter(){
+		return 2*(this.width+this.height);
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
+
+
+// const cc = new Rectangle(10,20)
+// console.log(cc.getArea())
+
+// const p = new Square(30,50)
+// console.log(p.getPerimeter());
