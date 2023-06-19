ff99666a3953402e11a40e5499a1eb97abb015bb
Dikesh Kumar <117905520+Dikesh01@users.noreply.github.com>
2023-06-19 17:37:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-//complete this code
 class Rectangle {
-	rect_w;
-	rect_h;
-	constructor(rect_w, rect_h){
-		this.rect_w = rect_w;
-		this.rect_h= rect_h;
-	}class Rectangle {
 constructor(width, height) {
 this._width = width;
 this._height = height;
@@ -48,34 +41,3 @@ console.log(square.getPerimeter()); // output: 28
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
-	get width(){
-		return this.rect_w;
-	}
-	get height(){
-		return this.rect_h;
-	}
-	getArea(){
-		return (this.rect_w*this.rect_h);
-	}
-}
-
-class Square extends Rectangle {
-	sq_s;
-	constructor(sq_s){
-		super(sq_s);
-	}
-	getPerimeter(){
-		return 4*(this.sq_s);
-	}
-	getArea()
-}
-
-// Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
-
-// const cc = new Rectangle(10,20)
-// console.log(cc.getArea())
-
-// const p = new Square(30)
-// console.log(p.getPerimeter());
